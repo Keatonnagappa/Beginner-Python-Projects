fc6361bcb7f1353330eff389d969ab30a2012ee6
Keatonnagappa <Keatonnagappa@gmail.com>
2021-12-30 19:04:10 +0200

    Generating band names using concatenation

---
+++ Day 1 - Band Name Generator.py
@@ -0,0 +1,13 @@
+# Band Name Generator
+
+# Greeting
+print("Welcome to the Band Name Generator.")
+
+# First Prompt
+city_name = input("What\'s the name of the city you grew up in? \n")
+
+# Second Prompt
+pet_name = input("What\'s the name of your pet?\n")
+
+# Print Band Name using both inputs
+print("Your band name could be: \n" + city_name + " " + pet_name)
