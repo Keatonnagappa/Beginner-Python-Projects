9466f1b6cf3d0c9dadb0115b92dc92fce05acc10
Keatonnagappa <Keatonnagappa@gmail.com>
2021-12-30 20:35:47 +0200

    Body Mass Index Calculator

--- Day 2 - BMI Calculator.py
+++ Day 2 - BMI Calculator.py
@@ -2,6 +2,6 @@ height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
 
 # BMI Formula
-bmi = float(weight) / float(height)**2
+bmi = float(weight) / (float(height)**2)
 # output
 print(int(bmi))
