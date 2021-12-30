97062db50bf64499fcc9f73a60a1fc5d16932c77
Keatonnagappa <Keatonnagappa@gmail.com>
2021-12-30 23:20:25 +0200

    Basic pizza ordering program

---
+++ Day 3 - Pizza Order.py
@@ -0,0 +1,29 @@
+# Prompt for input
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S,M,L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+# Calculations
+bill = 0
+
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    elif size == "M":
+        bill += 3
+    elif size == "L":
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+# Output
+print(f"Your final bill is ${bill}.")
