73713aa589825dd535acfcead8371c05656beffc
Keatonnagappa <Keatonnagappa@gmail.com>
2021-12-30 19:13:48 +0200

    Is the number odd or even?

---
+++ Day 3 - Odd or Even.py
@@ -0,0 +1,8 @@
+# prompting for input
+number = int(input("Which number do you want to check? "))
+
+# calculation and output
+if number % 2 == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
