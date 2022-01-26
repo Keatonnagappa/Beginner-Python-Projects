f5a23101429688a5596f949f3adc960f148ecfd8
Keatonnagappa <81191874+Keatonnagappa@users.noreply.github.com>
2022-01-26 02:27:20 +0200

    Creating The Popular Caesar Cipher Encryption Program

--- Day 8 - Caesar Cipher Encryption.py
+++ Day 8 - Caesar Cipher Encryption.py
@@ -26,7 +26,7 @@ while not should_end:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
-    # ensuring the shift never exceeds the max index of out alphabet list
+    # ensuring the shift never exceeds the max index of our alphabet list
     shift = shift % 26
 
     caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
